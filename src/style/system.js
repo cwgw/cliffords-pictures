1b8a5531ad451c12073a2beb9fc847eb0f700ce7
Charlie Wright <34384816+cwgw@users.noreply.github.com>
2019-10-29 21:19:48 -0700

    Dev (#1)
    * Simplify
    
    * Fix grid layout at small sizes

--- src/style/system.js
+++ src/style/system.js
@@ -9,15 +9,24 @@ const get = (scale, key) => props => {
   return sGet(theme, `${scale}.${key}`);
 };
 
-const getColor = color => props => get('colors', color)(props);
+const color = Object.keys(theme.colors).reduce(
+  (f, color) => {
+    f[color] = f(color)({ theme });
+    return f;
+  },
+  color => props => get('colors', color)(props)
+);
 
-const color = Object.keys(theme.colors).reduce((o, color) => {
-  o[color] = getColor(color)({ theme });
-  return o;
-}, getColor);
+const space = Object.keys(theme.space).reduce(
+  (f, n) => {
+    f[n] = get('space', n)({ theme });
+    return f;
+  },
+  n => props => get('space', n)(props)
+);
 
-const transparentize = (n, color) => props => {
-  return _transparentize(n, getColor(color)(props));
+const transparentize = (n, c) => props => {
+  return _transparentize(n, color(c)(props));
 };
 
-export { color, transparentize };
+export { color, space, transparentize };
