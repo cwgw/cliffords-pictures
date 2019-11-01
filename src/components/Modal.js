774718509ce3f0b62a2f6ebeea127ede042fc73d
Charlie Wright <charlie@cwgw.co>
2019-10-31 17:26:25 -0700

    Initial gesture controller init

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,12 +4,15 @@ import { jsx } from '@emotion/core';
 import css from '@styled-system/css';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
 import mousetrap from 'mousetrap';
+import { useSpring, animated } from 'react-spring';
+import { useGesture } from 'react-use-gesture';
+import Image from 'gatsby-image';
 
 import { space, transparentize } from 'style/system';
 
 import Button from 'components/Button';
 import AlbumContext from 'components/AlbumContext';
-import Photo from 'components/Photo';
+// import Photo from 'components/Photo';
 
 const Modal = () => {
   const {
@@ -20,6 +23,8 @@ const Modal = () => {
     prevPhoto,
   } = React.useContext(AlbumContext);
 
+  const [{ x }, setX] = useSpring(() => ({ x: 0 }));
+
   React.useEffect(() => {
     mousetrap.bind('left', prevPhoto);
     mousetrap.bind('right', nextPhoto);
@@ -29,6 +34,13 @@ const Modal = () => {
       mousetrap.unbind('right');
     };
   }, [nextPhoto, prevPhoto]);
+
+  const bind = useGesture({
+    onDrag: state => {
+      setX({ x: state.down ? state.movement[0] : 0 });
+      console.log(state);
+    },
+  });
 
   return (
     <DialogOverlay
@@ -56,6 +68,7 @@ const Modal = () => {
           marginX: 'auto',
           // overflow: 'hidden',
         })}
+        aria-label="Photo modal"
       >
         <Button
           onClick={closeModal}
@@ -64,22 +77,37 @@ const Modal = () => {
             top: 'sm',
             right: 'sm',
           })}
-        >
-          Close
-        </Button>
-        <div
+          children="Close"
+        />
+        <animated.div
           css={css({
             display: 'grid',
             gridTemplateColumns: 'repeat(3, 100%)',
-            gridColumnGap: 'lg',
-            transform: `translateX(calc(-100% - ${space.lg}px))`,
+            gridColumnGap: '32px',
           })}
+          style={{
+            transform: x.interpolate(
+              x => `translateX(calc(${x}px - 100% - 32px))`
+            ),
+          }}
+          {...bind()}
         >
           {currentPhoto &&
-            currentPhoto.map(({ id, image }) => (
-              <Photo key={id} image={image.full} />
-            ))}
-        </div>
+            currentPhoto.map((photo, i) =>
+              photo ? (
+                <div key={photo.id}>
+                  <Image
+                    fluid={photo.image.full}
+                    style={{
+                      pointerEvents: 'none',
+                    }}
+                  />
+                </div>
+              ) : (
+                <span key={i} />
+              )
+            )}
+        </animated.div>
       </DialogContent>
     </DialogOverlay>
   );
