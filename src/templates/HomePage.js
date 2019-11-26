a24ce0097e765e73e7dc18203ffac47288bfa7d8
Charlie Wright <charlie@cwgw.co>
2019-11-25 16:02:49 -0800

    Show total photos

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -6,10 +6,13 @@ import PhotoGrid from 'components/PhotoGrid';
 
 const Index = ({ pageContext }) => {
   const { photos } = React.useContext(AlbumContext);
-  const count = photos.length || pageContext.photos.length;
+  const current = photos.length || pageContext.photos.length;
+  const total = pageContext.photoTotal;
   return (
     <Layout>
-      <p style={{ textAlign: 'center' }}>{count} images</p>
+      <p style={{ textAlign: 'center' }}>
+        [{current}/{total}] images
+      </p>
       <PhotoGrid pageData={pageContext} isInfiniteScrollAllowed={true} />
     </Layout>
   );
